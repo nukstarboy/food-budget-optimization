03d9de18c9a721857b5e85691d5c1348b9e75f6a
nukstarboy <niki.iliev@abv.bg>
2022-10-06 00:48:09 +0300

    set-up-project

---
+++ src/main/java/com/example/eatinghealthy/EatingHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatinghealthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingHealthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatingHealthyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eatinghealthy/EatingHealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatinghealthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingHealthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
