c4876b5b78f58ffc03df038766826597186cce2d
nukstarboy <niki.iliev@abv.bg>
2023-01-03 21:19:29 +0200

    Stigler algorithm in progress

---
+++ src/main/java/models/Food.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Food {
+    public String name;
+    public String quantity;
+    public double price;
+    public double[] ingredients;
+
+    public Food(String name, String quantity, double price, double[] ingredients) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
---
+++ src/main/java/models/Nutrient.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Nutrient {
+    public String name;
+    public double quantity;
+
+    public Nutrient(String name, double quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
